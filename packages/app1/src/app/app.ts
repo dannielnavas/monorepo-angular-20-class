134e35563b3f25d512f1ab8195a6143c1b4fb60c
Danniel Navas <dannielnavas@gmail.com>
2025-10-14 07:36:52 -0500

    feat(app): add UiShared component to app1 and app2 templates and imports

--- packages/app1/src/app/app.ts
+++ packages/app1/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { UiShared } from '@monorepo-angular-20/ui-shared';
 import { NxWelcome } from './nx-welcome';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule, UiShared],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
